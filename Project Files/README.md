09a437b3f14c8e3cfcd0cb2fcc28f76a0e741454
SAI SUMANTH <160086817+sumanth3856@users.noreply.github.com>
2026-02-24 16:30:58 +0530

    Uploaded Project Files

---
+++ Project Files/README.md
@@ -0,0 +1,2 @@
+# ToyCraft-Tales
+ToyCraft Tales is a data intelligence application designed to uncover insights from US Toy Manufacturing Data. This project features comprehensive data visualizations that explore various aspects of the industry, including production trends, regional performance, and market dynamics.
---
+++ Project Files/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/story")
+def story():
+    return render_template("story.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
